e5f0e9a97565965b85325d21b156710f16b8297b
filipesantos1 <filipedsantos78@gmail.com>
2025-04-15 14:21:13 -0300

    Meu primeiro commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".iphone")
+
+
+function trocaCor(cor) {
+    
+    let circulo = document.querySelector (".circulo")
+
+circulo.style.background = cor
+
+
+}    
+
+function trocaImagem(imagem) {
+
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem
+}
+
+ 
+
+
+
